080a000285ac2d9c8899e8e508a3b416fc4e39fd
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-13 10:45:00 +0300

    added IsLikedNews class

--- news/permissions.py
+++ news/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 from rest_framework.permissions import BasePermission
-from news.models import UserRoles
+from news.models import UserRoles, News
 
 
 class IsAdminRole(BasePermission):
@@ -15,10 +15,22 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHOD:
             return True
-        return obj.author == request.user  # obj.user bazai userne
+        return obj.author == request.user
 
 
 class IsNewsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ['DELETE']:
             return obj.news.author == request.user
+
+
+class IsLikedNews(BasePermission):
+    message = 'You have already liked this news'
+
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            user = request.user
+            news_id = view.kwargs.get('pk')
+            news = News.objects.get(id=news_id)
+            return not news.like_set.filter(user=user).exists()
+        return True
