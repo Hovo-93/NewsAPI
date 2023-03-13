432a7903ce6baa86c6f6c1353d93275c3e241635
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-13 10:42:41 +0300

    added get_liked

--- news/serializers.py
+++ news/serializers.py
@@ -1,33 +1,44 @@
 from rest_framework import serializers
-from .models import News, Comment, Like
+from rest_framework.fields import CurrentUserDefault
+
+from .models import News, Comment, Like, User
+
+
+class UserRelatedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name')
 
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ('id', 'user', 'news')
+        fields = ('pk',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(source='user.username', read_only=True)
-    # news = serializers.StringRelatedField(source='news.title')#todo
+    user = UserRelatedSerializer()
 
     class Meta:
         model = Comment
-        fields = ('id', 'user', 'news', 'text', 'created_at')
+        fields = ('id', 'user', 'text', 'created_at')
 
 
 class NewsSerializer(serializers.ModelSerializer):
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
     likes = serializers.SerializerMethodField()
     comments = serializers.SerializerMethodField()
+    liked = serializers.SerializerMethodField()
 
     class Meta:
         model = News
-        fields = ('id', 'title', 'content', 'author', 'created_at', 'updated_at', 'likes', 'comments')
+        fields = ('id', 'title', 'content', 'author', 'created_at', 'updated_at', 'liked', 'likes', 'comments')
 
     def get_likes(self, obj):
-        return obj.like_set.count()  # like_set ete related name chenq greum djanfon default dnum e
+        return obj.like_set.count()
 
-    def get_comments(self,obj):
-        return  obj.comment_set.count()
+    def get_liked(self, obj):
+        return obj.like_set.filter(user=self.context['request'].user).count() > 0
+
+    def get_comments(self, obj):
+        return obj.comment_set.count()
