5820c1037a2e7dd1c57bbee1948c912f0494ab1a
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-13 10:43:16 +0300

    changed urls

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NewsList, NewsDetail, CommentDetail, CommentList, LikeDetail, LikeList
+from .views import NewsList, NewsDetail, CommentDetail, CommentList, LikeList
 
 app_name = 'news'
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path('news/<int:pk>/', NewsDetail.as_view()),
     path('news/<int:pk>/comments/', CommentList.as_view(), name='comment-list'),
     path('news/comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
-    path('news/<int:pk>/likes', LikeList.as_view(), name='like-list'),
-    path('news/likes/<int:pk>', LikeDetail.as_view(), name='like-detail'),
+    path('news/<int:pk>/like', LikeList.as_view(), name='like-list'),
 
 ]
