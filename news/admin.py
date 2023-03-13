b7e40bf5a8c102eb4e15fafdd4db3557fbdc9abc
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-13 10:43:59 +0300

    added UserAdmin in User

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import News,Comment,User,Like
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
+from .models import News, Comment, User, Like, UserRoles
+
+
 # Register your models here.
 #
 admin.site.register(News)
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
-
